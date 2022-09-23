89fc6314ffc109398087eb9abd2301e5fba48f44
William Lodea Magnabosco <williamlodea@gmail.com>
2022-09-22 23:49:08 -0400

    altas horas da noite

--- Especializar/TrabalhandoComAPIs/fetch-discover-api/main.js
+++ Especializar/TrabalhandoComAPIs/fetch-discover-api/main.js
@@ -7,8 +7,41 @@ function getUsers(){
     .catch(error => console.error(error))
 }
 
-getUsers()
+function getUser()  {
+    fetch(`${url}/18`)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userCity.textContent = data.city
+        userAvatar.src = data.avatar
+    })
+    .catch(error => console.error(error))
+}
 
+
+function addUser(newUser){
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
+const newUser = {
+    name: "William Magnabosco",
+    avatar: "http://lorempixel.com.br/400/200",
+    city: "Sinop"
+}
+
+addUser(newUser)
+
+getUsers()
+getUser()
 
 /*
 
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - Upload do certificado 
 - Javascript Assíncrono e Promises
 - Trabalhando com APIs... 22/09/2022 áS 13h35
+- Subindo exercícios com API
